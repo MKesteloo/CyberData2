cd10b2b8b8c576cfb932f0263f98348fc961d15d
MKesteloo <mitchellkesteloo@hotmail.com>
2018-06-01 15:17:51 +0200

    help

--- pca.R
+++ pca.R
@@ -1,10 +1,16 @@
+library(bio3d)
 data1 <- read.csv(file="C:/Users/mitch/Documents/GitHub/CyberData2_group30/set1.csv", header=TRUE, sep=",")
 data2 <- read.csv(file="C:/Users/mitch/Documents/GitHub/CyberData2_group30/set2.csv", header=TRUE, sep=",")
 
 #don't do pca on attack flag and the date
-pca.train <- data1[2:44]
-pca.test <- data2[2:44]
+train <- data1[2:44]
+test <- data2[2:44]
 
+#scale: mean 0 std 1 otherwise pca does not work well
+pca.train <- scale(train)
+pca.test <- scale(test)
+pca.train <- data.frame(pca.train)
+pca.test <- data.frame(pca.test)
 #remove always zero
 cols.dont.want <- c("S_PU3", "F_PU3", "S_PU5", "F_PU5", "S_PU9", "F_PU9", "S_PU1") 
 pca.train <- pca.train[, ! names(pca.train) %in% cols.dont.want, drop = F]
@@ -31,7 +37,7 @@ pca.test$S_V2 <- as.numeric(pca.test$S_V2)
 
 #compute components: by default it centers around mean 0
 #using scale = T means setting std to 1
-prin_comp <- prcomp(pca.train, scale. = T)
+prin_comp <- pca.xyz(pca.train)
 
 #compute std of each pc
 std_dev <- prin_comp$sdev
@@ -40,16 +46,11 @@ std_dev <- prin_comp$sdev
 pr_var <- std_dev^2
 
 #scree plot: take number of pc that retains as much variance as possible
-#plot created by lines below shows around 15~16 pcs should be used
+#plot created by lines below shows around 15~16 explain almost 100 percent variance
 prop_varex <- pr_var/sum(pr_var)
 plot(prop_varex, xlab = "Principal Component", ylab = "Proportion of Variance Explained", type = "b")
 
 #cumulative scree plot
 plot(cumsum(prop_varex), xlab = "Principal Component", ylab = "Cumulative Proportion of Variance Explained",
        type = "b")
-
-prin_comp_train <- prin_comp[1:16]
-prin_comp_test <- prcomp(pca.train, scale. = T)
-prin_comp_test <- prin_comp_test[1:16]
-
 
